026960d078111bb54e75efef07e15fd83d25e494
Susie <susie.odneal@gmail.com>
2019-05-25 12:34:09 -0500

    holy shit it's been a while.
    Just committing whatever is on my computer rn

---
+++ Assets/Scripts/randomize.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomize : MonoBehaviour {
+	public GameObject[] workouts;
+	int currentIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void NewWorkout(){
+		int newIndex = Random.Range (0, workouts.Length);
+		workouts [currentIndex].SetActive (false);
+		currentIndex = newIndex;
+		workouts [currentIndex].SetActive (true);
+	}
+
+}
